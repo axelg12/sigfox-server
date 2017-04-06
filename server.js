2dbd1af55f51dcf37fed3f4cb7b348e9fc2cc578
Axel Máni <selii93@gmail.com>
2017-04-06 11:15:39 +0200

    fully functional

--- server.js
+++ server.js
@@ -1,17 +1,77 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
+var redis = require("redis");
+var client = redis.createClient();
+var bodyParser = require('body-parser');
+
+var trashcans = [
+  {
+    name: "trashcan demo",
+    id: '1CC3A6',
+    lat: 55.385934,
+    lng: 10.403766,
+    status: "empty",
+    lastCheck: 1490367555,
+  },
+  {
+    name: "trashcan demo2",
+    id: '1CC3A7',
+    lat: 55.395425,
+    lng: 10.385417,
+    status: "empty",
+    lastCheck: 1490367555,
+  }
+];
+
+client.set("trashcans", JSON.stringify(trashcans));
+
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  client.get("trashcans", function(err, reply) {
+      // reply is null when the key is missing
+      res.send(JSON.parse(reply))
+  });
+});
+
+app.post('/empty', function(req, res) {
+  for (var i = 0; i < trashcans.length; i++) {
+    if (trashcans[i].id === String(req.body.id)) {
+      console.log('empty');
+      trashcans[i].status = "empty";
+      client.set("trashcans", JSON.stringify(trashcans));
+    }
+  }
+  res.send('OK');
+
 })
 
 app.get('/call', function (req, res) {
   console.log("new callback");
+  for (var i = 0; i < trashcans.length; i++) {
+    if (trashcans[i].id === String(req.query.id)) {
+      trashcans[i].status = "full";
+      trashcans[i].lastCheck = req.query.time;
+    }
+  }
+  client.set("trashcans", JSON.stringify(trashcans));
   console.log(req.query.id, req.query.time, req.query.lat, req.query.lng);
-  res.send("OK")
-})
+  // 1CC3A6 1490367555 55.0 10.0
+  res.send('OK');
+});
 
+app.get('/map', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(port, function () {
   console.log("App is running on port " + port);
---
+++ static/main.js
@@ -0,0 +1,45 @@
+var map;
+var markers = {};
+$(document).ready(function(){
+  map = new GMaps({
+    el: '#map',
+    lng: 10.385417,
+    lat: 55.395425
+  });
+  setInterval(checkSensors, 10000);
+  checkSensors();
+});
+
+function emptyTrash(id) {
+  console.log('sending', id);
+  $.post( "/empty", { id: id } );
+  markers[id].setIcon('https://www.google.com/mapfiles/marker_green.png');
+}
+
+function checkSensors() {
+  $.get( "/", function( data ) {
+    console.log('data', data.length, Object.keys(markers).length);
+    for (var i = 0; i < data.length; i++) {
+      if (Object.keys(markers).length < data.length) {
+        markers[data[i].id] = map.addMarker({
+          lat: data[i].lat,
+          lng: data[i].lng,
+          title: data[i].name,
+          icon: data[i].status === 'empty' ? 'https://www.google.com/mapfiles/marker_green.png' : '',
+          infoWindow: {
+            content:"<div class='marker'>" +
+                "ID:" + data[i].name +
+                "<div  onClick='emptyTrash(\x22"+data[i].id+"\x22)' class='clear'>Clear trash</div>" +
+              "</div>"
+          }
+        });
+      } else {
+        if (markers.hasOwnProperty(data[i].id)) {
+          console.log('status', data[i].status);
+          icon = data[i].status === 'empty' ? 'https://www.google.com/mapfiles/marker_green.png' : '';
+          markers[data[i].id].setIcon(icon);
+        }
+      }
+    }
+  });
+}
